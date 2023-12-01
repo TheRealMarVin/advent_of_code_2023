bc9c2cf00b27ae71858c6c4500b6a4f3898223ab
therealmarvin <nakedtoothbrush@hotmail.com>
2023-12-01 12:56:29 -0500

    create a template

---
+++ day_template/day_XX_01.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def main():
+    # Read input data from a file
+    with open('data.txt', 'r') as file:
+        calibration_input = file.readlines()
+
+
+if __name__ == "__main__":
+    print("Hello")
+    main()
+    print("Done")
