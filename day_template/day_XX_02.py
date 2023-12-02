a2d2748e9784d5500b7e9cfbaab1cd73ecd8a77f
therealmarvin <nakedtoothbrush@hotmail.com>
2023-12-02 07:18:12 -0500

    more generic name

--- day_template/day_XX_02.py
+++ day_template/day_XX_02.py
@@ -4,7 +4,7 @@ import numpy as np
 def main():
     # Read input data from a file
     with open('data.txt', 'r') as file:
-        calibration_input = file.readlines()
+        input_data = file.readlines()
 
 
 if __name__ == "__main__":
