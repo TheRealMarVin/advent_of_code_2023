33251fa26f91efdfec146ca37426e9055b61ebaa
therealmarvin <nakedtoothbrush@hotmail.com>
2023-12-13 20:57:10 -0500

    day 13

---
+++ day_13/day_13_02.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from day_13.prepare_data import prepare_data
+
+
+def find_equal_columns(grid):
+    for row in range(len(grid)):
+        rows_above = grid[row - 1::-1]
+        rows_below = grid[row:]
+
+        column_pairs = zip(rows_above, rows_below)
+        unequal_pairs_count = sum(c != d for pair in column_pairs for c, d in zip(*pair))
+        if unequal_pairs_count == 1:
+            return row
+    else:
+        return 0
+
+
+def main():
+    # Read input data from a file
+    input_data = prepare_data(use_dev_data=False)
+
+    res = 0
+    for index, grid in enumerate(input_data):
+
+        rows = find_equal_columns(grid)
+        columns = find_equal_columns(grid.T)
+
+        # print(columns_pairs, roes)
+        if (columns) > (rows):
+            res += (columns)
+        else:
+            res += (rows) * 100
+
+        print(index+1, '-', (rows), (columns), res)
+
+    print(res)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    main()
+    print("Done")
---
+++ day_13/prepare_data.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def prepare_data(use_dev_data=False):
+    # Read input data from a file
+    if use_dev_data:
+        with open('data_dev.txt', 'r') as file:
+            input_data = file.readlines()
+    else:
+        with open('data_challenge.txt', 'r') as file:
+            input_data = file.readlines()
+
+    input_data = [string.rstrip('\n') for string in input_data]
+    grids = []
+    curr_grid = []
+    for line in input_data:
+        if len(line) == 0:
+            grids.append(np.array(curr_grid))
+            curr_grid = []
+        else:
+            curr_grid.append(np.array(list(line)))
+
+    if len(curr_grid) > 0:
+        grids.append(np.array(curr_grid))
+
+    return grids
+
+
+if __name__ == "__main__":
+    print("Hello")
+    prepare_data(True)
+    print("Done")
